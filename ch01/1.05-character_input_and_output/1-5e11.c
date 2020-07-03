feb9a887a5b1d3b65e597f60d4363becf1ab06be
Erick Quintanilla <equintanilla@berkeley.edu>
2020-07-02 19:30:58 -0700

    Complete exercise 1-11

---
+++ ch01/1.05-character_input_and_output/1-5e11.c
@@ -0,0 +1,8 @@
+/**
+ * Exercise 1-11
+ * How would you test the word count program?
+ * What kinds of input are most likely to
+ * uncover bugs if there are any?
+ * '_' instead of ' ' causes issues; it does not
+ * correctly count spaces i.e ____bug____
+*/
