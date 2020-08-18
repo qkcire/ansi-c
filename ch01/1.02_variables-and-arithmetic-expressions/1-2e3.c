d829ea720fdd24714dfd2e5617e53a5a172d667c
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-18 14:56:50 -0700

    Complete exercise 1-3

---
+++ ch01/1.02_variables-and-arithmetic-expressions/1-2e3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Exercise 1-3
+ * Modify the temperature conversion
+ * program to print a heading above
+ * the table.
+*/
+
+int main() {
+  float fahr, celsius;
+  float lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  fahr = lower;
+  printf("Fahrenheit-Celsius\n");
+  printf("    Converter     \n");
+  printf("------------------\n");
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+  return 0;
+}
