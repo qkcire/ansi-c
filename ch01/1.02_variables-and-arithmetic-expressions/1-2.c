a3e90376e1b5deeebb4cc75ab819ed3a9c52f5d8
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-18 14:57:32 -0700

    Modify 1.2 to accomodate decimals

--- ch01/1.02_variables-and-arithmetic-expressions/1-2.c
+++ ch01/1.02_variables-and-arithmetic-expressions/1-2.c
@@ -6,15 +6,15 @@
 */
 
 int main() {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
   lower = 0;
   upper = 300;
   step = 20;
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
   return 0;
