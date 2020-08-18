c405c2da2dda097a9755669f030c53e9cae2e177
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-18 14:39:41 -0700

    Complete section 1.01

---
+++ ch01/1.01_getting-started/1-1e1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Exercise 1-1
+ * Run the "Hello, world" program on your system.
+ * Experiment with leaving out parts of the program,
+ * to see what error messages you get.
+*/
+
+int main() {
+  printf("Hello, world.\n")
+}
