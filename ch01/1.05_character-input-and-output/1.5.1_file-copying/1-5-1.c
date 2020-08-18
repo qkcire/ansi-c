90186e7832c190ea5170158cbf904fed7db66cb5
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-18 16:24:11 -0700

    Complete 1-5-1

--- ch01/1.05_character-input-and-output/1.5.1_file-copying/1-5-1.c
+++ ch01/1.05_character-input-and-output/1.5.1_file-copying/1-5-1.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+/**
+ * Copy input to output; 1st version
+*/
+
+int main() {
+  int c;
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
+  }
+  return 0;
+}
