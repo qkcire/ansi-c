282c1616647555ff0bf802081a61ae6b947c33aa
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-20 00:19:39 -0700

    Initialize 1.9

---
+++ ch01/1.09_character-arrays/1-9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* max input line length */
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/**
+ * print the longest input line
+*/
+
+int main() {
+  
+  return 0;
+}
