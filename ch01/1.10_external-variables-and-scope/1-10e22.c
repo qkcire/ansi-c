3dc76ae5fb0b7037cf9d45c42e933ee7fa11edf8
Erick Quintanilla <equintanilla@berkeley.edu>
2020-08-21 23:40:14 -0700

    Initiate exercise 1-22

---
+++ ch01/1.10_external-variables-and-scope/1-10e22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Exercise 1-22
+ * Write a program 'fold' long input lines into two
+ * or more shorter lines after the last non-blank
+ * character that occurs before the n-th column of
+ * input. Make sure your program does something 
+ * intelligent with very long lines, and if there are
+ * not blanks or tabs before the specified column.
+*/
+
+int main() {
+  return 0;
+}
